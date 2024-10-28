cfe5f45e7ee0cd4a23c8afc628420fe014ce3688
siretitus <titusarchives@gmail.com>
2024-10-29 01:55:12 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import CountryDetail from './src/components/CountryDetail';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CountryDetail" component={CountryDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
